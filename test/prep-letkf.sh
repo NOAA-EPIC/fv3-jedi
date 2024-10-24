3a1d84257492ef72473fc4c817260820ebe604fe
Mark A Potts <mark.potts@noaa.gov>
2024-10-24 11:08:31 -0400

    fixes from merge

--- test/prep-letkf.sh
+++ test/prep-letkf.sh
@@ -3,10 +3,15 @@
 # work on a generic system and generate the experiment directory that is used to run the ufs forecast regression tests.
 set -x
 
+mkdir -p Data/ModelRunDirs/c48_001
+mkdir -p Data/ModelRunDirs/c48_002
+cp  Data/ModelRunDirs/UFS_warmstart_1/input.nml  Data/ModelRunDirs/c48_001
+
 echo "&ensemble_nml
  ensemble_size = 2
 /
-" >> Data/ModelRunDirs/UFS_warmstart_1/input.nml
+" >> Data/ModelRunDirs/c48_001/input.nml
+cp  Data/ModelRunDirs/c48_001/input.nml  Data/ModelRunDirs/c48_002
 
 cd Data/ModelRunDirs/UFS_warmstart_1/RESTART
 for file in *tile*.nc; do new=`echo $file | sed 's/tile/ens_01.tile/g'`; ln -s $file $new; done
